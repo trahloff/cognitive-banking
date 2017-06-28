b64c71e088c15f6701423750f35d7a5610c25676
Tobias Rahloff <rahloff@gmail.com>
2017-06-28 23:40:18 +0200

    cleaned up overview ctrl

--- cb-frontend/public/components/overview.ctrl.js
+++ cb-frontend/public/components/overview.ctrl.js
@@ -1,39 +1,35 @@
 angular
     .module('overviewCtrls', [])
     .controller('overviewCtrl', ($scope, $http) => {
-      $scope.increment = () => {
-        $scope.value++
-      }
-
-      $scope.decrement = () => {
-        $scope.value--
-      }
-
       const load = () => {
-        $scope.line_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'Dezember']
-        $scope.line_series = ['Series A', 'Series B']
+        $scope.line = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+          series: ['Series A', 'Series B'],
+          data: [
+            [65, 59, 80, 81, 56, 55, 40, 10, 80, 30, 70, 40],
+            [28, 48, 40, 19, 86, 27, 90, 20, 20, 40, 25, 65]
+          ]
+        }
 
-        $scope.line_data = [
-                [65, 59, 80, 81, 56, 55, 40, 10, 80, 30, 70, 40],
-                [28, 48, 40, 19, 86, 27, 90, 20, 20, 40, 25, 65]
-        ]
+        $scope.pie = {
+          labels: ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'],
+          series: ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'],
+          data: [350, 450, 100]
+        }
 
-        $scope.pie_labels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
-        $scope.pie_series = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
-        $scope.pie_data = [350, 450, 100]
+        $scope.radar = {
+          labels: ['Food', 'Drugs', 'Alcohol', 'Tech', 'Watches', 'ScamCoins', 'Whatever'],
+          data: [[65, 90, 90, 81, 56, 55, 40]]
+        }
 
-        $scope.radar_labels = ['Food', 'Drugs', 'Alcohol', 'Tech', 'Watches', 'ScamCoins', 'Whatever']
-        $scope.radar_data = [
-                [65, 90, 90, 81, 56, 55, 40]
-        ]
-
-        $scope.bar_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-        $scope.bar_series = ['2016', '2017']
-
-        $scope.bar_data = [
-                [65, 59, 80, 81, 56, 55, 40],
-                [28, 48, 40, 19, 86, 27, 90]
-        ]
+        $scope.bar = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          series: ['2016', '2017'],
+          data: [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+          ]
+        }
       }
 
       ;(init => {
@@ -41,4 +37,8 @@ angular
           load()
         }, 0)
       })()
+
+      $scope.changeYear = () => {
+        alert()
+      }
     })
