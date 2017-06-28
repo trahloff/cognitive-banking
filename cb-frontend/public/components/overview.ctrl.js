4fc1cd184f7f362d52493c8bcc4a899e25a5901c
Tobias Rahloff <rahloff@gmail.com>
2017-06-29 00:34:49 +0200

    minor style changes

--- cb-frontend/public/components/overview.ctrl.js
+++ cb-frontend/public/components/overview.ctrl.js
@@ -2,6 +2,8 @@ angular
     .module('overviewCtrls', [])
     .controller('overviewCtrl', ($scope, $mdDialog) => {
       const load = () => {
+        $scope.spendingYear = 2016
+
         $scope.line = {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'Dezember'],
           series: ['Series A', 'Series B'],
@@ -30,48 +32,59 @@ angular
             [28, 48, 40, 19, 86, 27, 90]
           ]
         }
-      }
-
-      const showPrompt = () => {
-        // const confirm = $mdDialog.prompt()
-        //                   .title('What would you name your dog?')
-        //                   .textContent('Bowser is a common name.')
-        //                   .placeholder('Change Year')
-        //                   .initialValue('Buddy')
-        //                   .ok('Okay!')
-        //                   .hasBackdrop(false)
-
-        var confirm = $mdDialog.confirm()
-.parent(angular.element(document.body))
-.title('Would you like to delete your debt?')
-.content('All of the banks have agreed to forgive you your debts.')
-.ariaLabel('Lucky day')
-.ok('Please do it!')
-.cancel('Sounds like a scam')
-.hasBackdrop(false)
-
-        $mdDialog.show(confirm).then(result => {
-          console.log('You decided to name your dog ' + result + '.')
-        })
+        // $scope.bar_data = [
+        //   [65, 59, 80, 81, 56, 55, 40],
+        //   [28, 48, 40, 19, 86, 27, 90]
+        // ]
       }
 
       $scope.showPrompt = function (ev) {
     // Appending dialog to document.body to cover sidenav in docs app
-        var confirm = $mdDialog.prompt()
-      .title('What would you name your dog?')
-      .textContent('Bowser is a common name.')
-      .placeholder('Dog name')
-      .ariaLabel('Dog name')
-      .initialValue('Buddy')
-      .targetEvent(ev)
-      .hasBackdrop(false)
-      .ok('Okay!')
-      .cancel('I\'m a cat person')
+        const confirm = $mdDialog.prompt()
+                        .title('Change Year')
+                        .textContent('Compare your spendings in 2017 to:')
+                        .placeholder('Dog name')
+                        .initialValue($scope.spendingYear)
+                        .targetEvent(ev)
+                        .hasBackdrop(false)
+                        .ok('Okay!')
 
-        $mdDialog.show(confirm).then(function (result) {
-          $scope.status = 'You decided to name your dog ' + result + '.'
-        }, function () {
-          $scope.status = 'You didn\'t name your dog.'
+        $mdDialog.show(confirm).then(result => {
+          const parsedResult = Number(result)
+
+          $scope.spendingYear = parsedResult
+
+          switch (parsedResult) {
+            case 2015:
+              $scope.bar = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                series: ['2015', '2017'],
+                data: [
+              [25, 49, 60, 65, 76, 75, 20],
+              [28, 48, 40, 19, 86, 27, 90]
+                ]
+              }
+              break
+            case 2014:
+              $scope.bar = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                series: ['2014', '2017'],
+                data: [
+                [15, 29, 30, 95, 56, 75, 60],
+                [28, 48, 40, 19, 86, 27, 90]
+                ]
+              }
+              break
+            default:
+              $scope.bar = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                series: ['2016', '2017'],
+                data: [
+              [65, 59, 80, 81, 56, 55, 40],
+              [28, 48, 40, 19, 86, 27, 90]
+                ]
+              }
+          }
         })
       }
 
