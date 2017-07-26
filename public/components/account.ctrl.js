0878857c4ff33d6ee72224900c28fb36c01e116c
Tobias Rahloff <rahloff@gmail.com>
2017-07-26 22:05:37 +0200

    minor changes

--- public/components/account.ctrl.js
+++ public/components/account.ctrl.js
@@ -1,7 +1,6 @@
 angular
 .module('accountCtrls', [])
 .controller('accountCtrl', ($scope, $rootScope) => {
-  $scope.user = {lastEvents: '- 06/06 12:03:11 Fraud detected\n\n- 06/06 19:53:02 Forecast for Client #198714 generated'}
 })
 .config(['$mdThemingProvider', $mdThemingProvider => {
   // Configure a dark theme with primary foreground yellow
